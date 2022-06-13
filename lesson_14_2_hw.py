9f13253c2149d86d11453be15ac9c5295319b84d
alexandr-stanchik <lex023ott@mail.ru>
2022-06-13 23:07:56 +0300

    first commit

---
+++ lesson_14_2_hw.py
@@ -0,0 +1,62 @@
+class Pet:
+    def __init__(self, name, age, master):
+        self.name = name
+        self.age = age
+        self.master = master
+
+    def run(self):
+        return f'{self.name} is running'
+
+    def jump(self):
+        return f'{self.name} is jumping'
+
+    def birthday(self):
+        self.age += 1
+
+    def sleep(self):
+        return f'{self.name} is sleeping'
+
+
+class Dog(Pet):
+    def __init__(self, name, age, master, height, weight):
+        super().__init__(name, age, master)
+        self.height = height
+        self.weight = weight
+
+    def bark(self):
+        return f'{self.name} is barking'
+
+
+class Cat(Pet):
+    def meow(self):
+        return f'{self.name} is meowing'
+
+
+class Parrot(Pet):
+    def fly(self):
+        return f'{self.name} is flying'
+
+
+dog = Dog('Max', 5, 'Jon', 50, 10)
+print(dog.run())
+print(dog.jump())
+dog.birthday()
+print(f'{dog.name} age {dog.age}')
+print(dog.sleep())
+print(dog.bark())
+
+cat = Cat('Felix', 3, 'Alice')
+print(cat.run())
+print(cat.jump())
+cat.birthday()
+print(f'{cat.name} age {cat.age}')
+print(cat.sleep())
+print(cat.meow())
+
+parrot = Parrot('Ruby', 1, 'Oliver')
+print(parrot.run())
+print(parrot.jump())
+parrot.birthday()
+print(f'{parrot.name} age {parrot.age}')
+print(parrot.sleep())
+print(parrot.fly())
